424c44827f9953eb59c0d7560b022c13cdce5dd4
Guthrie <stuka101@gmail.com>
2020-01-25 22:49:48 -0800

    feat: Add page structure

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `rgba(36, 71, 71, 1)`,
       marginBottom: `1.45rem`,
     }}
   >
