e50473006105b8a61341cad4d2d224c2bfd0de67
Guthrie <stuka101@gmail.com>
2020-01-26 00:10:24 -0800

    feat: Add color styles

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -19,10 +19,7 @@ const Header = ({ siteTitle }) => (
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
+          className="text-accent"
         >
           {siteTitle}
         </Link>
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'primary': '#244747',
+        'primary-hover': '#3a7c76',
+        'secondary': '#5a301f',
+        'secondary-hover': '#733d28',
+        'accent': '#eab932',
+        'accent-hover': '#d1b056'
+      }
+    }
   },
   variants: {},
   plugins: []
