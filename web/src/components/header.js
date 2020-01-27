03f9f8a26a8cdb8ebe284d70b346c8d52a9d8589
Guthrie <stuka101@gmail.com>
2020-01-26 23:14:13 -0800

    feat: Add fonts

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -2,7 +2,7 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
+const Header = ({ siteTitle, menuLinks }) => (
   <header
     style={{
       background: `rgba(36, 71, 71, 1)`,
@@ -16,14 +16,30 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          className="text-accent"
-        >
+      <h1>
+        <Link to="/" className="text-accent">
           {siteTitle}
         </Link>
       </h1>
+      <div>
+        <nav>
+          <ul style={{ display: "flex", flex: 1 }}>
+            {menuLinks.map(link => (
+              <li
+                key={link.name}
+                style={{
+                  listStyleType: `none`,
+                  padding: `1rem`,
+                }}
+              >
+                <Link className="text-accent" to={link.link}>
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </div>
   </header>
 )
