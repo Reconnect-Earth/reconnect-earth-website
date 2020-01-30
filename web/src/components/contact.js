72c06b8fe921495a9858efcf68ab031c579166d5
Guthrie <stuka101@gmail.com>
2020-01-30 00:23:24 -0800

    feat: Add form bot field & netlify forms parsing hook

--- web/src/components/contact.js
+++ web/src/components/contact.js
@@ -1,25 +1,44 @@
-import React from "react";
+import React from "react"
 
-const Contact = () => ( <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+const Contact = () => (
+  <form
+    name="contact"
+    method="POST"
+    data-netlify="true"
+    netlify-honeypot="bot-field"
+  >
+    <p>
+      <label>
+        Your Name: <input type="text" name="name" />
+      </label>
+    </p>
+    <p>
+      <label>
+        Your Email: <input type="email" name="email" />
+      </label>
+    </p>
+    <p>
+      <label>
+        I'm Interested In:{" "}
+        <select name="role[]" multiple>
+          <option value="trips">Trips</option>
+          <option value="volunteering">Volunteering</option>
+          <option value="something-else">Something Else</option>
+        </select>
+      </label>
+    </p>
+    <p>
+      <label>
+        Message: <textarea name="message"></textarea>
+      </label>
+    </p>
+    <p>
+      <input type="hidden" name="bot-field" />
+    </p>
+    <p>
+      <button type="submit">Send</button>
+    </p>
+  </form>
 )
 
 export default Contact
