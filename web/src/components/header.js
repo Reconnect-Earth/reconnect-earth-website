bf4268a96e839f0309e845b2c4ecba51c2fb5dee
Guthrie <stuka101@gmail.com>
2020-01-29 21:56:37 -0800

    refactor: Remove 'Home' link & prettify

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -2,46 +2,35 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-
 const Header = ({ siteTitle, menuLinks }) => (
-  <header
-    style={{
-      background: `rgba(36, 71, 71, 1)`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+  <header className="bg-primary mb-5 flex">
+    <nav className="flex">
       <h1>
-        <Link id="site-title" to="/" className="text-accent text-6xl">
+        <Link
+          id="site-title"
+          to="/"
+          className="text-accent text-3xl whitespace-no-wrap p-4"
+        >
           {siteTitle}
         </Link>
       </h1>
-      <div>
-        <nav>
-          <ul style={{ display: "flex", flex: 1 }}>
-            {menuLinks.map(link => (
-              <li
-                key={link.name}
-                style={{
-                  listStyleType: `none`,
-                  padding: `1rem`,
-                }}
-              >
-                <Link id="menu-link" className="text-accent text-3xl" to={link.link}>
-                  {link.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
-    </div>
+      <ul className="flex flex-wrap items-end mb-3">
+        {menuLinks.map(link => (
+          <li
+            className="list-none pr-4 pl-4 whitespace-no-wrap"
+            key={link.name}
+          >
+            <Link
+              id="menu-link"
+              className="text-accent text-2xl"
+              to={link.link}
+            >
+              {link.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
   </header>
 )
 
