09fdccf77cb29846cfb82fbef7ae25bfad6a0e2c
Guthrie <stuka101@gmail.com>
2020-01-31 00:28:51 -0800

    fix: Add styling
    WIP form component

--- web/src/components/contact.js
+++ web/src/components/contact.js
@@ -6,21 +6,23 @@ const Contact = () => (
     method="POST"
     data-netlify="true"
     netlify-honeypot="bot-field"
+    className="bg-accent flex flex-col items-center"
   >
+    <h1>Contact Us</h1>
     <p>
       <label>
-        Your Name: <input type="text" name="name" />
+        Your Name: <input className="block" type="text" name="name" />
       </label>
     </p>
     <p>
       <label>
-        Your Email: <input type="email" name="email" />
+        Your Email: <input className="block" type="email" name="email" />
       </label>
     </p>
     <p>
       <label>
         I'm Interested In:{" "}
-        <select name="role[]" multiple>
+        <select className="block" name="role[]" multiple>
           <option value="trips">Trips</option>
           <option value="volunteering">Volunteering</option>
           <option value="something-else">Something Else</option>
@@ -29,14 +31,14 @@ const Contact = () => (
     </p>
     <p>
       <label>
-        Message: <textarea name="message"></textarea>
+        Message: <textarea className="block" name="message"></textarea>
       </label>
     </p>
     <p>
       <input type="hidden" name="bot-field" />
     </p>
     <p>
-      <button type="submit">Send</button>
+      <button className="bg-primary text-lg px-4 py-2 rounded shadow-2xl m-4 text-accent hover:text-accent-hover hover:bg-primary-hover" type="submit">Submit</button>
     </p>
   </form>
 )
